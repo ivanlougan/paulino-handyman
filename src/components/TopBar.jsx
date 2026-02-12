fb2eb4ae7dcfc934b1e14912b5c60ec957038a97
Robert Zdeb <robinio123@gmail.com>
2026-02-12 01:01:23 +0000

    added spacing into TopBar component

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,7 @@ function TopBar() {
   return (
     <div className="topbar">
       <div className="topbar-content">
-       Phone: 07777 777 777 | email: paulinoboss@gmail.com
+       phone: 07777 777 777 <span className="bar"> | </span> email: paulinoboss@gmail.com
       </div>
     </div>
   )
