bc38865381369ec6f82066cff78a0e56dc96c518
Robert Zdeb <robinio123@gmail.com>
2026-02-12 02:58:47 +0000

    adjusted topbar styles for mobile

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,7 @@ function TopBar() {
   return (
     <div className="topbar">
       <div className="topbar-content">
-       phone: 07777 777 777 <span className="bar"> | </span> email: paulinoboss@gmail.com
+       07777 777 777 <span className="bar"> | </span> paulinoboss@gmail.com
       </div>
     </div>
   )
